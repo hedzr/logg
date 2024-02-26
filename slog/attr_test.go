56f2ab926b6ffb748ff21ff21e52e24a2a6b2ec6
hz <hedzrz@gmail.com>
2024-02-27 07:22:13 +0800

    cov: for attr

--- slog/attr_test.go
+++ slog/attr_test.go
@@ -13,3 +13,70 @@ func TestNewGroupedAttr(t *testing.T) {
 	ga.SetValue(NewAttrs("i3", 3))
 	t.Logf("ga: %v", ga)
 }
+
+var testBytes []byte // test data; same as testString but as a slice.
+
+func TestKvp_SerializeValueTo(t *testing.T) {
+	pc := NewPrintCtx(testBytes)
+	k := &kvp{"k1", 1}
+
+	k.SerializeValueTo(pc)
+
+	t.Logf("%v", pc.String())
+}
+
+func TestGkvp_SetValue(t *testing.T) {
+	pc := NewPrintCtx(testBytes)
+
+	g := gkvp{
+		key:   "g1",
+		items: nil,
+	}
+
+	// g.SetValue(1)
+	// g.SerializeValueTo(pc)
+	// t.Logf("%v", pc.String())
+
+	g.Add(String("s1", "hello"))
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+
+	g.SetValue(String("s1", "hello"))
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+
+	g.SetValue([]Attr{
+		String("s1", "world"),
+	})
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+
+	g.SetValue(NewAttrs(
+		String("s1", "hello"),
+	))
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+
+	// json mode
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	pc.jsonMode = true
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+
+	// and no color
+	testBytes = testBytes[:0]
+	pc = NewPrintCtx(testBytes)
+	pc.jsonMode = true
+	pc.noColor = true
+	g.SerializeValueTo(pc)
+	t.Logf("%v", pc.String())
+}
