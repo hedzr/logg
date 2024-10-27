0d9bf47b5cf2dbd6ab2f7b01cb439174cddcd63e
hz <hedzrz@gmail.com>
2024-10-27 08:25:59 +0800

    add small example app

---
+++ _examples/small/go.mod
@@ -0,0 +1,17 @@
+module github.com/hedzr/logg/examples/small
+
+go 1.22.7
+
+replace github.com/hedzr/logg => ../../
+
+require (
+	github.com/hedzr/is v0.5.29
+	github.com/hedzr/logg v0.7.1
+)
+
+require (
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
+)
--- _examples/small/main.go
+++ _examples/small/main.go
@@ -12,11 +12,41 @@ import (
 	"github.com/hedzr/is/basics"
 	"github.com/hedzr/is/term"
 	"github.com/hedzr/is/term/color"
+
+	logz "github.com/hedzr/logg/slog"
 )
 
 func main() {
 	defer basics.Close()
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	testIs(ctx)
+	testLogz(ctx)
+
+	catcher := is.Signals().Catch()
+	catcher.
+		WithPrompt("Press CTRL-C to quit...").
+		WithOnLoop(dbStarter, cacheStarter, mqStarter).
+		WithOnSignalCaught(func(sig os.Signal, wg *sync.WaitGroup) {
+			println()
+			slog.Info("signal caught", "sig", sig)
+			cancel() // cancel user's loop, see Wait(...)
+		}).
+		Wait(func(stopChan chan<- os.Signal, wgDone *sync.WaitGroup) {
+			slog.Debug("entering looper's loop...")
+			go func() {
+				// to terminate this app after a while automatically:
+				time.Sleep(10 * time.Second)
+				stopChan <- os.Interrupt
+			}()
+			<-ctx.Done()  // waiting until any os signal caught
+			wgDone.Done() // and complete myself
+		})
+}
+
+func testIs(ctx context.Context) {
 	is.RegisterStateGetter("custom", func() bool { return is.InVscodeTerminal() })
 
 	println("state.InTesting:   ", is.InTesting())
@@ -47,26 +77,60 @@ func main() {
 	println("term.GetTtySizeByFile(stdout): ", rows, cols, err)
 	println("term.IsStartupByDoubleClick:   ", term.IsStartupByDoubleClick())
 
-	ctx, cancel := context.WithCancel(context.Background())
-	catcher := is.Signals().Catch()
-	catcher.
-		WithPrompt("Press CTRL-C to quit...").
-		WithOnLoop(dbStarter, cacheStarter, mqStarter).
-		WithOnSignalCaught(func(sig os.Signal, wg *sync.WaitGroup) {
-			println()
-			slog.Info("signal caught", "sig", sig)
-			cancel() // cancel user's loop, see Wait(...)
-		}).
-		Wait(func(stopChan chan<- os.Signal, wgDone *sync.WaitGroup) {
-			slog.Debug("entering looper's loop...")
-			go func() {
-				// to terminate this app after a while automatically:
-				time.Sleep(10 * time.Second)
-				stopChan <- os.Interrupt
-			}()
-			<-ctx.Done()  // waiting until any os signal caught
-			wgDone.Done() // and complete myself
-		})
+	logz.InfoContext(ctx, "pre-forecasting") // by default this line shall not be displayed since logz (logg/slog) is in WarnLevel.
+}
+
+func testLogz(ctx context.Context) {
+	logz.SetLevel(logz.DebugLevel)
+
+	logz.InfoContext(ctx, "Hello, world!")
+
+	logz.InfoContext(ctx, "Hello", "target", "world")
+	logz.Default().SetColorMode(false)
+	logz.InfoContext(ctx, "Hello", "target", "world")
+	logz.Default().SetJSONMode(true)
+	logz.InfoContext(ctx, "Hello", "target", "world")
+
+	testLogzAdapter(ctx)
+}
+
+func testLogzAdapter(ctx context.Context) {
+	l := logz.New("standalone-logger-for-app",
+		logz.NewAttr("attr1", 2),
+		logz.NewAttrs("attr2", 3, "attr3", 4.1),
+		"attr4", true, "attr3", "string",
+		logz.WithLevel(logz.AlwaysLevel),
+	)
+	defer l.Close()
+
+	sub1 := l.New("sub1").With("logger", "sub1")
+	sub2 := l.New("sub2").With("logger", "sub2").
+		WithLevel(logz.InfoLevel) // a new child instance here
+
+	// create a log/slog logger HERE
+	logger := slog.New(logz.NewSlogHandler(l, &logz.HandlerOptions{
+		NoColor:  false,
+		NoSource: true,
+		JSON:     false,
+		Level:    logz.DebugLevel,
+	}))
+
+	l.Infof("logger: %v", logger)
+	l.Infof("l: %v", l)
+
+	// and logging with log/slog
+	logger.DebugContext(ctx, "hi debug", "AA", 1.23456789)
+	logger.InfoContext(ctx,
+		"incoming request",
+		slog.String("method", "GET"),
+		slog.String("path", "/api/user"),
+		slog.Int("status", 200),
+	)
+
+	// now using our logg/slog interface
+	sub1.DebugContext(ctx, "hi debug", "AA", 1.23456789)
+	sub2.DebugContext(ctx, "hi debug", "AA", 1.23456789)
+	sub2.InfoContext(ctx, "hi info", "AA", 1.23456789)
 }
 
 func dbStarter(stopChan chan<- os.Signal, wgDone *sync.WaitGroup) {
