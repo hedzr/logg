2c13f8aa1fca100f66839cf52ebaebd1e0742751
hz <hedzrz@gmail.com>
2024-11-19 07:11:04 +0800

    add TestErrors

--- bench/slog_test.go
+++ bench/slog_test.go
@@ -2,8 +2,12 @@ package bench
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/hedzr/is"
 
 	slogg "github.com/hedzr/logg/slog"
 )
@@ -30,6 +34,7 @@ func TestUseJSON(t *testing.T) {
 
 	ctx, msg, attrs := context.Background(), getMessage(0), fakeLoggArgs()
 	logger.InfoContext(ctx, msg, attrs...)
+	logger.Println()
 
 	logger.SetJSONMode(false)
 	logger.InfoContext(ctx, msg, attrs...)
@@ -38,11 +43,39 @@ func TestUseJSON(t *testing.T) {
 	logger.InfoContext(ctx, msg, attrs...)
 }
 
-func Test3(t *testing.T) {
+func TestErrors(t *testing.T) {
 	ctx, msg, attrs := context.Background(), getMessage(0), fakeLoggArgs()
 	logger := newLoggTextMode().Set(attrs...).
 		SetWriter(os.Stdout).SetLevel(slogg.InfoLevel)
 	logger.InfoContext(ctx, msg)
+
+	err := errors.New("test error")
+	logger.Error("error occurred", "err", err)
+
+	logger = newLoggTextMode().SetWriter(os.Stdout).SetLevel(slogg.InfoLevel)
+
+	is.SetDebugMode(true)
+
+	logger.SetColorMode(false)
+	logger.Error("error occurred", "testing", is.InTesting(), "err", err)
+
+	logger.SetColorMode()
+	logger.Error("error occurred", "err", err)
+
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.SetColorMode(false)
+				logger.Error("panic caught", "testing", is.InTesting(), "err", err)
+				logger.SetColorMode()
+				logger.Error("panic caught", "err", err)
+			}
+		}()
+
+		panic("panic test")
+	}()
+
+	fmt.Printf("err: %+v\n", err)
 }
 
 func Test4(t *testing.T) {
