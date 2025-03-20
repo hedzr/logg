02b3c26a92e77d36e11959e21b714463afa2abdd
hz <hedzrz@gmail.com>
2025-03-21 07:16:20 +0800

    bump to v0.8.5

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -3,5 +3,5 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "logg/slog" // appName of hedzr/logg package
-const version = "v0.8.1"    // version of hedzr/logg package | update it while bumping hedzr/logg' version
+const version = "v0.8.5"    // version of hedzr/logg package | update it while bumping hedzr/logg' version
 const Version = version     // version name you can check it
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ go 1.23.7
 // replace github.com/hedzr/go-utils/v2 => ./
 
 require (
-	github.com/hedzr/is v0.7.1
+	github.com/hedzr/is v0.7.5
 	gopkg.in/hedzr/errors.v3 v3.3.5
 )
 
