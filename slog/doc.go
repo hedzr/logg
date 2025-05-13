f78a842e30dd9f0f759f16ed26afc06ee756a05e
hz <hedzrz@gmail.com>
2025-05-13 20:21:05 +0800

    bump to v0.8.16

--- slog/doc.go
+++ slog/doc.go
@@ -1,5 +1,5 @@
 package slog
 
-const version = "v0.8.15" // new Version of logg/slog
+const version = "v0.8.16" // new Version of logg/slog
 const Version = version   // version name you can check it
 const appName = "logg/slog"
