f6166b1fa219f66b2ee52c9228f7ef9155232076
hz <hedzrz@gmail.com>
2025-06-24 07:46:29 +0800

    fix small example

--- examples/small/main.go
+++ examples/small/main.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/hedzr/is"
-	"github.com/hedzr/is/basics"
 	"github.com/hedzr/is/term"
 	"github.com/hedzr/is/term/color"
 
@@ -18,24 +17,23 @@ import (
 )
 
 func main() {
-	defer basics.Close()
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	testIs(ctx)
+	testIs2(ctx)
 	testLogz(ctx)
 
 	catcher := is.Signals().Catch()
 	catcher.
 		WithPrompt("Press CTRL-C to quit...").
-		WithOnLoopFunc(dbStarter, cacheStarter, mqStarter).
-		WithOnSignalCaught(func(sig os.Signal, wg *sync.WaitGroup) {
+		// WithOnLoopFunc(dbStarter, cacheStarter, mqStarter).
+		WithOnSignalCaught(func(ctx context.Context, sig os.Signal, wg *sync.WaitGroup) {
 			println()
 			slog.Info("signal caught", "sig", sig)
 			cancel() // cancel user's loop, see <-ctx.Done() in Wait(...)
 		}).
-		WaitFor(func(closer func()) {
+		WaitFor(ctx, func(ctx context.Context, closer func()) {
 			slog.Debug("entering looper's loop...")
 			defer closer()
 			// to terminate this app after a while automatically:
@@ -76,6 +74,74 @@ func testIs(ctx context.Context) {
 	println("term.IsStartupByDoubleClick:   ", term.IsStartupByDoubleClick())
 
 	logz.InfoContext(ctx, "pre-forecasting") // by default this line shall not be displayed since logz (logg/slog) is in WarnLevel.
+}
+
+func testIs2(ctx context.Context) {
+	// start a color text builder
+	var c = color.New()
+	var pos color.CursorPos
+
+	// paint and get the result (with ansi-color-seq ready)
+	var result = c.Println().
+		Color16(color.FgRed).Printf("[1st] hello, %s.", "world").
+		Println().
+		SavePosNow().
+		Println("XX").
+		Color16(color.FgGreen).Printf("hello, %s.\n", "world").
+		Color256(160).Printf("[160] hello, %s.\n", "world").
+		Color256(161).Printf("[161] hello, %s.\n", "world").
+		Color256(162).Printf("[162] hello, %s.\n", "world").
+		Color256(163).Printf("[163] hello, %s.\n", "world").
+		Color256(164).Printf("[164] hello, %s.\n", "world").
+		Color256(165).Printf("[165] hello, %s.\n", "world").
+		UpNow(4).Echo(" ERASED ").
+		RightNow(11).
+		CursorGet(ctx, &pos).
+		RGB(211, 211, 33).Printf("[16m] hello, %s. pos=%+v", "world", pos).
+		Println().
+		RestorePosNow().
+		Println("ZZ").
+		DownNow(8).
+		Println("DONE").
+		Build()
+
+		// and render the result
+	fmt.Println(result)
+
+	// another colorful builfer
+	c = color.New()
+	fmt.Println(c.Color16(color.FgRed).
+		Printf("[2nd] hello, %s.", "world").Println().Build())
+
+	// cursor operations
+	c = color.New()
+	c.SavePosNow()
+	// fmt.Println(c.CursorSavePos().Build())
+
+	fmt.Print(c.
+		Printf("[3rd] hello, %s.", "world").
+		Println().
+		Color256(163).Printf("[163] hello, %s.\n", "world").
+		Color256(164).Printf("[164] hello, %s.\n", "world").
+		Color256(165).Printf("[165] hello, %s.\n", "world").
+		Build())
+
+	fmt.Print("0")         // now, col = 1
+	c.UpNow(2)             //
+	fmt.Print("ABC")       // embedded "ABC" into "[]"
+	c.CursorGet(ctx, &pos) //
+	c.RightNow(2)          // to be overwrite "hello"
+	fmt.Print("HELLO")     //
+
+	c.RestorePosNow()
+	c.DownNow(1)
+	fmt.Print("T") // write "T" to beginning of "[163]" line
+
+	c.DownNow(4)
+
+	// color.Down(4)
+	// color.Left(1)
+	fmt.Printf("\nEND (pos = %+v)\n", pos)
 }
 
 func testLogz(ctx context.Context) {
@@ -203,24 +269,6 @@ func testLogzAdapter(ctx context.Context) {
 	sub1.DebugContext(ctx, "hi debug", "AA", 1.23456789)
 	sub2.DebugContext(ctx, "hi debug", "AA", 1.23456789)
 	sub2.InfoContext(ctx, "hi info", "AA", 1.23456789)
-}
-
-func dbStarter(closer func()) {
-	defer closer()
-	// initializing database connections...
-	// ...
-}
-
-func cacheStarter(closer func()) {
-	defer closer()
-	// initializing redis cache connections...
-	// ...
-}
-
-func mqStarter(closer func()) {
-	defer closer()
-	// initializing message queue connections...
-	// ...
 }
 
 const (
