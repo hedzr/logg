36d92a5ecf31b357517e4316a7ab51d589d945ec
hz <hedzrz@gmail.com>
2025-07-27 08:30:53 +0800

    bump to v0.8.47
      - added `ResetDefault()`
      - improved LogWriters' handwriting
      - upgrade deps
        - is - added more os & arch detector such as `BSD()`

--- slog/doc.go
+++ slog/doc.go
@@ -1,5 +1,5 @@
 package slog
 
-const version = "v0.8.46" // new Version of logg/slog
+const version = "v0.8.47" // new Version of logg/slog
 const Version = version   // version name you can check it
 const appName = "logg/slog"
