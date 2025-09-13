1f231d72b00fc250e7e59a470e7716db4c2521bc
hz <hedzrz@gmail.com>
2025-09-14 07:16:28 +0800

    bump to v0.8.60
      - upgrade deps & security patch

--- slog/doc.go
+++ slog/doc.go
@@ -1,5 +1,5 @@
 package slog
 
-const version = "v0.8.55" // new Version of logg/slog
+const version = "v0.8.60" // new Version of logg/slog
 const Version = version   // version name you can check it
 const appName = "logg/slog"
