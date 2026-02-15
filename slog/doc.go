664abe9550209100c45ef11fd2dca4fe7de3c1e5
hz <hedzrz@gmail.com>
2026-02-15 08:24:19 +0800

    bump to v0.9.0 - upgrade go toolchain to 1.25.0

--- slog/doc.go
+++ slog/doc.go
@@ -1,7 +1,7 @@
 package slog
 
 const (
-	version = "v0.8.68" // new Version of logg/slog
-	Version = version   // version name you can check it
+	version = "v0.9.0" // new Version of logg/slog
+	Version = version  // version name you can check it
 	appName = "logg/slog"
 )
