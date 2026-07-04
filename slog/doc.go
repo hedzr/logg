6c92e0cfb8062e60721138215e938201e64e9f01
hz <hedzrz@gmail.com>
2026-07-05 07:16:22 +0800

    bump to v0.9.3   - upgrade deps & security patch

--- slog/doc.go
+++ slog/doc.go
@@ -1,7 +1,7 @@
 package slog
 
 const (
-	version = "v0.9.0" // new Version of logg/slog
+	version = "v0.9.3" // new Version of logg/slog
 	Version = version  // version name you can check it
 	appName = "logg/slog"
 )
